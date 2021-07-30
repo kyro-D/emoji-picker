d0a9e84c47bca9e7f58dac8b6162dc0e599d4704
kyro-D <kyle.rose@colorado.edu>
2021-07-30 10:14:42 -0600

    add heroku site

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,6 @@
 module.exports = {
   siteMetadata: {
-    siteUrl: "https://www.yourdomain.tld",
+    siteUrl: "https://kdr-emoji-picker.herokuapp.com/",
     title: "emoji picker",
   },
   plugins: [
